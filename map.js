3545d8ef2f110ecdac163511ba5567a567146751
nyanmark <limanmark2003@gmail.com>
2021-12-02 18:31:55 +0300

    Initial Commit

---
+++ map.js
@@ -0,0 +1,14 @@
+var map = new jsVectorMap({
+    selector: "#map",
+    map: "world",
+    markers: [
+        { name: "Meppel, NL", coords: [52.6921, 6.1937], style: { fill: 'blue' } },
+        { name: "Roost, LU", coords: [49.7866, 6.0753], style: { fill: 'blue' } },
+        { name: "New York, US", coords: [40.7128, -74.0060], style: { fill: 'blue' } },
+        { name: "Las Vegas, US", coords: [36.1699, -115.1398], style: { fill: 'blue' } },
+        { name: "Miami, US", coords: [25.7617, -80.1918], style: { fill: 'blue' } },
+        { name: "Vancouver, CA", coords: [49.2827, -123.1207], style: { fill: 'blue' } },
+        { name: "Auckland, NZ", coords: [-36.8509, 174.7645], style: { fill: 'blue' } },
+        { name: "Frankfurt, DE", coords: [50.1109, 8.6821], style: { fill: 'blue' } }
+    ]
+});
