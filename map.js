a34cbf7f8859efed22503dbbbc7b226da0f61e1f
nyanmark <limanmark2003@gmail.com>
2021-12-04 22:46:15 +0300

    New Location

--- map.js
+++ map.js
@@ -9,6 +9,7 @@ var map = new jsVectorMap({
         { name: "Miami, US", coords: [25.7617, -80.1918], style: { fill: 'blue' } },
         { name: "Vancouver, CA", coords: [49.2827, -123.1207], style: { fill: 'blue' } },
         { name: "Auckland, NZ", coords: [-36.8509, 174.7645], style: { fill: 'blue' } },
-        { name: "Frankfurt, DE", coords: [50.1109, 8.6821], style: { fill: 'blue' } }
+        { name: "Frankfurt, DE", coords: [50.1109, 8.6821], style: { fill: 'blue' } },
+        { name: "St Petersburg, RU", coords: [59.9311, 30.3609], style: { fill: 'blue' } }
     ]
 });
